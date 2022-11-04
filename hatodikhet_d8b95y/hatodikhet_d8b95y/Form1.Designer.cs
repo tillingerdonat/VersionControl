96e018dbf466f341054e608edafc4880dc77d402
tillingerdonat <donat.tillinger@stud.uni-corvinus.hu>
2022-11-04 13:08:55 +0100

    félkész

--- hatodikhet_d8b95y/hatodikhet_d8b95y/Form1.Designer.cs
+++ hatodikhet_d8b95y/hatodikhet_d8b95y/Form1.Designer.cs
@@ -30,12 +30,48 @@ namespace hatodikhet_d8b95y
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.AutoSize = true;
+            this.mainPanel.Location = new System.Drawing.Point(25, 32);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(750, 406);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
